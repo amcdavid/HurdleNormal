b1f43a169e91f2632c937634d7dd8982130ddd9a
Andrew McDavid <amcdavid@fhcrc.org>
2015-02-02 17:23:14 -0800

    simulation code still somewhat borked

--- R/HurdleStructure.R
+++ R/HurdleStructure.R
@@ -1,6 +1,6 @@
 HurdleStructure <- function(G=NULL, H=NULL, K=NULL, sample=NULL, gibbs=NULL, estimate=NULL){
     if(!is.null(G)){
-        .checkArgs(G, H, K)
+        .checkArgs(G=G, H=H, K=K)
     } else if(!is.null(sample) || !is.null(gibbs)){
         p <- if(!is.null(sample)) ncol(sample) else ncol(gibbs)
         G <- H <- K <- matrix(NA, nrow=ncol(sample), ncol=ncol(sample))
@@ -13,16 +13,22 @@ HurdleStructure <- function(G=NULL, H=NULL, K=NULL, sample=NULL, gibbs=NULL, est
     }
 
     li <- list(true=list(G=G, H=H, K=K), sample=sample, gibbs=gibbs, estimate=estimate)
-    class(li, 'HurdleStructure')
+    class(li) <- c('list', 'HurdleStructure')
+    li
 }
 
 getGibbs <- function(hs, Nt=2000, ...){
     if(!is.null(hs$gibbs)) message("Replacing gibbs sample")
     gibbs <- with(hs$true, rGibbsHurdle(G, H, K, Nt=Nt, ...))
     hs$gibbs <- gibbs
+    hs
 }
 
-simulateHurdle210 <- function(N, p, dependence='G', structure='independence', structureArgs=list(sparsity=.1, groupwise=FALSE), intensityArgs=list(G=1, Hupper=1, Hlower=1, K=-.3), Hdiag=5, Kdiag=1, GmarginalProb=.5, Gdiag){
+getDensity <- function(hs, ...){
+    function(x) with(hs$true, dHurdle210(x, G, H, K, ...))
+}
+
+simulateHurdle210 <- function(N, p, dependence='G', structure='independence', structureArgs=list(sparsity=.1, groupwise=FALSE), intensityArgs=list(G=1, Hupper=1, Hlower=1, K=-.3), Hdiag=5, Kdiag=1, G0odds=0){
     dependence <- match.arg(dependence, c('G', 'Hupper', 'Hlower', 'K'), several.ok=TRUE)
     structure <- match.arg(structure, c('independence', 'sparse', 'chain'))
     Hlower <- Hupper <- diag(Hdiag, p)/2
@@ -51,55 +57,31 @@ simulateHurdle210 <- function(N, p, dependence='G', structure='independence', st
     K <- K+t(K)
     H <- Hupper + t(Hlower)
     G <- G+t(G)
-    ## now try to guess at the marginal distribution by replacing xI and x
-    ## in the conditional distributions with their expectations given independence
-    ex <- diag(H)/diag(K)
-    exI <- rep(.75, p)
-    K0 <- K
-    Hlower0 <-Hlower
-    Hupper0<- Hupper
-    diag(K0) <- diag(Hlower0) <- diag(Hupper0) <- 0
-    
-    guessMarginals <- function(Gdiag){
-        G0 <- G
-        diag(G0) <- 0
-        for(i in seq_len(10)){
-            gbaNoDiag <- crossprod(G0, exI)+ crossprod(Hlower0, ex*exI)
-            hba <- diag(H) + crossprod(Hupper0, exI) + #only H's diagonal
-                -crossprod(K0, ex*exI)/2 # not K's diagonal
-            logitP <- gbaNoDiag-.5*log(diag(K)/(2*pi))+hba^2/(2*diag(K))
-            ex <- hba/diag(K)
-            Gdiag <- pmax(pmin(logit(GmarginalProb)-logitP, 1000), -1000)
-            exI <- expit(logitP+Gdiag)
-            print(paste0('ex=', ex))
-            print(paste0('exI=',t(exI)))
-            print(paste0('Gdiag=', t(Gdiag)))
-        }
-        if(any(abs(exI-GmarginalProb) > .05)) warning("Wasn't able to match marginal probabilities")
-        Gdiag
-    }
-    if(missing(Gdiag)){
-        diag(G) <- guessMarginals(diag(G))
-    } else{
-        diag(G) <- Gdiag
-    }
+    addIndices <- .additiveVstate(ncol(H))
+    norm <- .calcNormalizing(addIndices$states, H=H, K=K)
     browser()
-    samp <- rGibbsHurdle(G, H, K, Nt=20*N, thin=.1)
+    G[addIndices$indices] <- -log(norm$N)
+
+    Gprime <- matrix(c(-13.4, (-37.5 + 26.8)/2, 0,
+                       (-37.5 + 26.8)/2, -13.4, 0,
+                       0, 0, -13.4), nrow=3, ncol=3)
+    samp <- rGibbsHurdle(Gprime, H, K, Nt=20*N, thin=.1)
     pairs(samp)
 
 }
 
 
-Kdep <- simulateHurdle210(200, 3, 'K', 'sparse', Gdiag=c(-19, -19, -13.41))
-Gdep <- simulateHurdle210(200, 3, 'G', 'sparse')
-Hupperdep <- simulateHurdle210(2000, 3, 'Hupper', 'sparse', intensityArgs=list(Hupper=1, Hlower=1))
-g12 <- glm(samp[,1] >0 ~ samp[,2]+I(samp[,2]>0), family='binomial')
-b12 <- lm(samp[,1] ~ samp[,2] + I(samp[,2]>0), subset=samp[,1]>0)
-g21 <- glm(samp[,2] >0 ~ samp[,1]+I(samp[,1]>0), family='binomial') #discrete significant
-b21 <- lm(samp[,2] ~ samp[,1] + I(samp[,1]>0), subset=samp[,2]>0) 
 
-Hlowerdep <- simulateHurdle210(2000, 3, 'Hlower', 'sparse', intensityArgs=list(Hupper=1, Hlower=1))
-g12 <- glm(samp[,1] >0 ~ samp[,2]+I(samp[,2]>0), family='binomial') 
-b12 <- lm(samp[,1] ~ samp[,2] + I(samp[,2]>0), subset=samp[,1]>0) #discrete significant
-g21 <- glm(samp[,2] >0 ~ samp[,1]+I(samp[,1]>0), family='binomial') #continuous significant
-b21 <- lm(samp[,2] ~ samp[,1] + I(samp[,1]>0), subset=samp[,2]>0) 
+##Kdep <- simulateHurdle210(200, 3, 'K', 'sparse', Gdiag=c(-19, -19, -13.41))
+## Gdep <- simulateHurdle210(200, 3, 'G', 'sparse')
+## Hupperdep <- simulateHurdle210(2000, 3, 'Hupper', 'sparse', intensityArgs=list(Hupper=1, Hlower=1))
+## 
+## 
+##  #discrete significant
+## 
+
+## Hlowerdep <- simulateHurdle210(2000, 3, 'Hlower', 'sparse', intensityArgs=list(Hupper=1, Hlower=1))
+## g12 <- glm(samp[,1] >0 ~ samp[,2]+I(samp[,2]>0), family='binomial') 
+## b12 <- lm(samp[,1] ~ samp[,2] + I(samp[,2]>0), subset=samp[,1]>0) #discrete significant
+## g21 <- glm(samp[,2] >0 ~ samp[,1]+I(samp[,1]>0), family='binomial') #continuous significant
+## b21 <- lm(samp[,2] ~ samp[,1] + I(samp[,1]>0), subset=samp[,2]>0) 
