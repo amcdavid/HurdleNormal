5bb140bb4c26abd09b40ff5897e451b6adfc8fa0
Andrew McDavid <amcdavid@fhcrc.org>
2015-02-13 02:46:29 -0800

    solve for initial lambda and return kkt conditions w/ solution

--- R/GroupLasso.R
+++ R/GroupLasso.R
@@ -20,32 +20,64 @@
 ##' @return matrix of parameters, one row per lambda
 ##' @useDynLib HurdleNormal
 ##' @importFrom Rcpp sourceCpp
-cgpaths <- function(theta, y.zif, this.model, lambda=exp(seq(1, -5, length.out=10)), control=list(tol=1e-6, maxrounds=300, maxit=500, debug=1)){
-    out <- matrix(NA, nrow=length(lambda), ncol=length(theta))
-    p <- (length(theta)+1)/4
-    colnames(out) <- parmap(p)
-    rownames(out) <- lambda
-    jerr <- rep(0, length(lambda))
-    for(l in seq_along(lambda)){
-        sp <-solvePen(theta, lambda[l], y.zif, this.model, control)
-        out[l,] <- sp
-        if(control$debug>0) message('Lambda = ', lambda[l], 'rounds = ', attr(sp, 'flag')['round'])
-        ## update jerr?
-    }
-    out
-}
+cgpaths <- function(y.zif, this.model, nlambda=100, lambda, lambda.min.ratio=if(length(y.zif)<ncol(this.model)) .005 else .05, control=list(tol=1e-5, maxrounds=300, maxit=500, debug=1)){
+    defaultControl <- list(tol=1e-6, maxrounds=300, maxit=500, debug=0)
+    nocontrol <- setdiff(names(defaultControl), names(control))
+    control[nocontrol] <- defaultControl[nocontrol]
+    p <- ncol(this.model)+1
+    n <- nrow(this.model)
+    blocks <- sapply(seq_len(p), function(x) which(x==coordmap(p)))
+    theta <- coordmap(p)
+    theta[theta!=1] <- 0
 
-solvePen <- function(theta, lambda, y.zif, this.model, control){
-    hl <- HurdleLikelihood(y.zif, this.model, theta=theta, lambda=lambda)
+    ## get likelihood object
+    hl <- HurdleLikelihood(y.zif, this.model, theta=theta, lambda=0)
     subll <- function(b, th) hl$LL(th,b)
     sg <- function(b, th) hl$grad(th,b, penalize=TRUE)
-    grad <- hl$gradAll
     ## use to test if gradient at 0 lies within lambda
     sg0 <- function(b){
         hl$grad(rep(0, 4), b, penalize=FALSE)
     }
-    p <- (length(theta)+1)/4   #actually p + 1
-    blocks <- sapply(seq_len(p), function(x) which(x==coordmap(p)))
+    LLall <- function(th) hl$LLall(th)
+    ## value for empty sol
+    l0 <- getLambda0(theta, subll, sg, sg0, control, p, blocks)
+    if(missing(lambda)) lambda <- 10^seq(log10(1.05*l0), log10(l0*lambda.min.ratio), length.out=nlambda)
+ #                                            
+  #                                           if(p<n) 3 else (n/p), length.out=nlambda)
+    out <- matrix(NA, nrow=length(lambda), ncol=length(theta))
+    kktout <- matrix(NA, nrow=length(lambda), ncol=p)
+    flout <- matrix(NA, nrow=length(lambda), ncol=4)
+    colnames(out) <- parmap(p)
+    rownames(kktout) <- rownames(flout) <- rownames(out) <- lambda
+    jerr <- rep(0, length(lambda))
+
+    
+    for(l in seq_along(lambda)){
+        hl$setLambda(lambda[l])
+        sp <-solvePen(theta, lambda[l], control, p, blocks, subll, sg, sg0, LLall)      
+        theta <- out[l,] <- sp
+        kktout[l,] <- attr(sp, 'kkt')
+        flout[l,] <- attr(sp, 'flag')
+        if(control$debug>0) message('Lambda = ', lambda[l], 'rounds = ', attr(sp, 'flag')['round'])
+        activeset <- sum(kktout[l,]>0)
+        if(activeset >= n/2) break
+        ## update jerr?
+    }
+    list(path=out, kktout=kktout, flout=flout)
+}
+
+getLambda0 <- function(theta, subll, sg, sg0, control, p, blocks){
+    subtheta <- theta[blocks[[1]]]
+    o <- optim(subtheta, subll, sg, b=1, method='BFGS')
+    o <- optim(subtheta, subll, sg, b=1, method='BFGS')
+    theta[blocks[[1]]] <- o$par
+    subll(1, theta[blocks[[1]]])
+    subgrad <- sapply(2:p, function(b) sqrt(sum(sg0(b)^2)))
+    max(subgrad)
+}
+
+solvePen <- function(theta, lambda, control, p, blocks,  subll, sg, sg0, LLall){
+    feval <- geval <- 0
     round <- 0
     converged <- FALSE
     while(!converged){
@@ -55,21 +87,33 @@ solvePen <- function(theta, lambda, y.zif, this.model, control){
             ## test if setting block to 0 is minimum
             subtheta <- theta[blocks[[b]]]
             if(b != 1){
+                geval <- geval +1
                 if(sqrt(sum(sg0(b)^2)) < lambda){
                     theta[blocks[[b]]] <- 0
-                    if(control$debug>2) print(noquote(paste0('Zeroed block ', b, ', grad0 = ', sqrt(sum(sg0()^2)))))
+                    if(control$debug>2) print(noquote(paste0('Zeroed block ', b, ', grad0 = ', sqrt(sum(sg0(b)^2)))))
                     next
                 } else if(sum(subtheta^2)<control$tol){
                     subtheta[] <- control$tol
                 }
             }
             oo <- optim(subtheta, subll, sg, b=b, method='BFGS', control=control['maxit'])
+            feval <- feval+oo$counts['function']
+            geval <- geval+oo$counts['gradient']
             theta[blocks[[b]]] <- oo$par
         } # end blockupdates
-        converged <- mean((theta-theta0)^2)<control$tol || round > control$maxrounds
+        converged <- mean((theta-theta0)^2)<(control$tol)^2 || round > control$maxrounds
         if(control$debug > 0){
-            if(control$debug>1 || (round %% 10)==0) print(noquote(paste0('penll=', round(hl$LLall(theta), 4), ' theta= ', paste(round(theta, 2), collapse=','))))
+            if(control$debug>1 || (round %% 10)==0) print(noquote(paste0('penll=', round(LLall(theta), 4), ' theta= ', paste(round(theta, 2), collapse=','))))
         }
     } # end main loop
-    structure(ifelse(abs(theta)<control$tol, 0, theta), flag=c(converged=converged, round=round))
+
+    kkt <- sapply(seq_len(p), function(b){
+        subtheta <- theta[blocks[[b]]]
+        if(all(abs(subtheta)<control['tol'])){
+            if(sqrt(sum(sg0(b)^2))<lambda) return(0L) else return(-99L)
+        } 
+        else return(sum(sg(b, subtheta)^2))
+    })
+        geval <- geval+p
+    structure(ifelse(abs(theta)<control$tol, 0, theta), flag=c(converged=converged, round=round, geval=geval, feval=feval), kkt=kkt)
 }
--- tests/testthat/test-GroupLasso.R
+++ tests/testthat/test-GroupLasso.R
@@ -7,14 +7,14 @@ H[1,2] <- 2
 context('Fit one penalized')
 rgh <- rGibbsHurdle(G, H, K, 5e3, 1e3)
 test_that('Can fit', {
-    theta <- c(gbb=-10, 0, 0, hbb=10, 0, 0, 0, 0, 0, 0, 1)+.02
-    names(theta) <- parmap(3)
     y.zif <- rgh[,1]
     this.model <- rgh[,-1,drop=FALSE]
-    cgpaths(theta, y.zif, this.model, lambda=c(8, 1, .5, .2, .05, 1e-6), control=list(tol=1e-8, maxrounds=200, debug=3))
+    theta <- c(gbb=-10, 0, 0, hbb=10, 0, 0, 0, 0, 0, 0, 1)+.02
+    names(theta) <- parmap(3)
+    paths <- cgpaths(y.zif, this.model, nlambda=20, control=list(tol=1e-6, maxrounds=200, debug=3))
 
     hm <- HurdleLikelihood(y.zif, this.model, theta=theta, lambda=0)
     thetareg <- optim(theta, hm$LLall, hm$gradAll, method='BFGS')
-    expect_equivalent(cgpaths[5,], thetareg$par)
+    expect_equivalent(paths[5,], thetareg$par)
 
 })
