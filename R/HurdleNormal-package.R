cd8dfe8340a07d15f97fddbee66296b4b4681ddc
Andrew McDavid <amcdavid@fhcrc.org>
2017-05-16 01:30:15 -0400

    doc updates

---
+++ R/HurdleNormal-package.R
@@ -0,0 +1,19 @@
+##' Single Cell RNAseq Expression Data from Shalek (2014)
+##'
+##' The 1-hour LPS data for genes with at least 30% expression
+##' after thresholding with \code{MAST} are reproduced here as a list containing:
+##'
+##' \itemize{
+##' \item exprs: Genes x Cells list of log+1 transformed TPM
+##' \item cdata: \code{data.frame} of cellular covariates
+##' \item fdata' \code{data.frame} of transcript covariates
+##' }
+##' @importFrom graphics plot
+"shalek2014_list"
+
+##'  A Hurdle model is a modification of a distribution at zero. This
+##'    package provides routines to estimate and sample from multivariate Hurdle models
+##'    on a Normal density. These are distributions that are conditionally Normal, but with singularities along the coordinate axes, so generalize a univariate zero-inflated distribution.
+##' The main functionality is to estimate graphical models using group-lasso penalized neighborhood selection.
+##' @references https://arxiv.org/abs/1610.05857
+"_PACKAGE"
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ These are distributions that are conditionally Normal, but with singularities al
 The main functionality is to estimate graphical models using group-lasso penalized neighborhood selection.
 
 ## Vignette
-A vignette with an example will be available soon.
+A vignette with an example is located [here](https://github.com/amcdavid/HurdleNormal/inst/doc/singlecell-networks.html)
 
 ## Citation
 [Graphical Models for Zero-Inflated Single Cell Gene Expression](https://arxiv.org/abs/1610.05857)
